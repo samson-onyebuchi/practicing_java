a5e824fcbfe495cc3418b19518739071083f01f5
samson-onyebuchi <nnadisamson63@gmail.com>
2024-06-18 16:29:19 +0100

    implemented the increament button

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-document.getElementById("count-el").innerText = 5
-//let count = 5
-//count = count + 1
-//console.log(count)
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    
+    }
